0e22f324677a254c07c17c9286c06eab9e5733a2
zarif007 <zarifhuq786@gmail.com>
2023-06-14 00:21:27 +0600

    ADDING: more dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
-import express, { Request, Response } from 'express'
-import envConfig from './config/envConfig';
-const app: Application = express();
+import express, { Request, Response, Express } from 'express'
+const app: Express = express()
+import cors from 'cors'
 
+// Cors setup
+    app.use(cors())
 
-app.listen(envConfig.PORT, () => {
-    console.log(`App is flying on PORT ${envConfig.PORT}`)
-})
+// Parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hell')
+  res.send('Hell')
 })
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
-import mongoose from "mongoose";
-import envConfig from "./config/envConfig";
+import mongoose from 'mongoose'
+import envConfig from './config/envConfig'
+import app from './app'
 
 const main = async () => {
   try {
-    await mongoose.connect(envConfig.database_url as string);
-    console.log(`🤩 Database is connected ${envConfig.PORT}`)
+    await mongoose.connect(envConfig.database_url as string)
+    console.log(`🤩 Database is connected`)
+
+    app.listen(envConfig.PORT, () => {
+      console.log(`App is listening on PORT ${envConfig.PORT}`)
+      console.log(`Process ID ${process.pid}`)
+    })
   } catch (err) {
     console.log(`Failed to connect to Database ${err}`)
   }
-};
+}
 
-main();
+main()
